0207cc90ba2082d2eeb475c0a88bae7241da2427
iman <144808588+iman41k@users.noreply.github.com>
2024-05-19 18:38:29 +0300

    Update CrappyArrayInventory.h

--- tests/CrappyArrayInventory.h
+++ tests/CrappyArrayInventory.h
@@ -1,22 +1,21 @@
-#ifndef _CRAPPYARRAYINVENTORY_H_
-#define _CRAPPYARRAYINVENTORY_H_
-
 #include <string>
 
-struct Item 
-{
-	std::string name;
-	int value;
+struct Item {
+    std::string name;
+    int value;
 };
 
-class CrappyArrayInventory 
-{
+class CrappyArrayInventory {
 public:
-	CrappyArrayInventory();
-	Item* getInventory();
-	int getInventorySize();
-private:
-	Item* items;
-};
+    CrappyArrayInventory();
+    CrappyArrayInventory(Item* array, int size);
+    constexpr CrappyArrayInventory(const CrappyArrayInventory& other);
+    constexpr CrappyArrayInventory(CrappyArrayInventory&& other);
 
-#endif /* _CRAPPYARRAYINVENTORY_H_ */
+    Item* getInventory();
+    int getInventorySize();
+
+private:
+    static const int MAX_INVENTORY_SIZE = 2;
+    Item inventory[MAX_INVENTORY_SIZE];
+};
