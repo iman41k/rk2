78eeec866f2376699f28558f1129f33a90207c67
iman <144808588+iman41k@users.noreply.github.com>
2024-05-19 20:21:56 +0300

    Update

--- CrappyArrayInventory.cpp
+++ CrappyArrayInventory.cpp
@@ -2,28 +2,28 @@
 
 CrappyArrayInventory::CrappyArrayInventory()
 {
-	//create some items
-	Item* array = new Item[2];
-	
-	Item* item = new Item();
-	item->name = "Sword";
-	item->value = 100;
-	array[0] = *item;
-	
-	item = new Item();
-	item->name = "Shield";
-	item->value = 50;
-	array[1] = *item;
-	
-	this->items = array;
+    //create some items
+    Item* array = new Item[2];
+
+    Item* item = new Item();
+    item->name = "Sword";
+    item->value = 100;
+    array[0] = *item;
+
+    item = new Item();
+    item->name = "Shield";
+    item->value = 50;
+    array[1] = *item;
+
+    this->items = array;
 }
 
 Item* CrappyArrayInventory::getInventory()
 {
-	return this->items;
+    return this->items;
 }
 
 int CrappyArrayInventory::getInventorySize()
 {
-	return 2;
+    return 2;
 }
